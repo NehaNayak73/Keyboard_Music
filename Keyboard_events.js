6d254588afb836ce700edfd166c90633de108bb5
Neha Nayak <160911567+NehaNayak73@users.noreply.github.com>
2024-10-01 12:33:26 +0530

    Update

---
+++ Keyboard_events.js
@@ -0,0 +1,65 @@
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 65) {
+      document.getElementById('Music_a').play();
+      var insideText = document.getElementById("inside_textA")
+      insideText.style.boxShadow= ("5px 0px 10px rgb(248, 199, 5)")
+      
+
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 83) {
+      document.getElementById('Music_s').play();
+      var insideText = document.getElementById("inside_textS")
+      insideText.style.boxShadow = ("5px 0px 10px rgb(248, 199, 5)")
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 68) {
+      document.getElementById('Music_d').play();
+      var insideText = document.getElementById("inside_textD")
+      insideText.style.boxShadow =("5px 0px 10px rgb(248, 199, 5)")
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 70) {
+      document.getElementById('Music_f').play();
+      var insideText = document.getElementById("inside_textF")
+      insideText.style.boxShadow = ("5px 0px 10px rgb(248, 199, 5)")
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 71) {
+      document.getElementById('Music_g').play();
+      var insideText = document.getElementById("inside_textG")
+      insideText.style.boxShadow = ("5px 0px 10px rgb(248, 199, 5)")
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 72) {
+      document.getElementById('Music_h').play();
+      var insideText = document.getElementById("inside_textH")
+      insideText.style.boxShadow = ("5px 0px 10px rgb(248, 199, 5)")
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 74) {
+      document.getElementById('Music_j').play();
+      var insideText = document.getElementById("inside_textJ")
+      insideText.style.boxShadow= ("5px 0px 10px rgb(248, 199, 5)")
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 75) {
+      document.getElementById('Music_k').play();
+      var insideText = document.getElementById("inside_textK")
+      insideText.style.boxShadow = ("5px 0px 10px rgb(248, 199, 5)")
+    }
+  });
+  document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 76) {
+      document.getElementById('Music_l').play();
+      var insideText = document.getElementById("inside_textL")
+      insideText.style.boxShadow =("5px 0px 10px rgb(248, 199, 5)")
+    }
+  });
